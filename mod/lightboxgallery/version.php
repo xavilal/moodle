2395564d2f74082cf74ac9e4141670e0290340d1
xavila <xavilal@gmail.com>
2012-08-15 13:48:52 +0100

    Eliminar tot

--- mod/lightboxgallery/version.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    $module->version  = 2011111600;  // The current module version (Date: YYYYMMDDXX)
-    $module->requires = 2007101509;  // Requires this Moodle version
-    $module->cron     = 0;           // Period for cron to check this module (secs)
-    $module->release = '1.0.0';
-    $module->maturity = MATURITY_STABLE;
-?>
